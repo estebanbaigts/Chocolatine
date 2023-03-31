6deab0c8fe6e311dd080ccb5e63758239088eaa6
esteban <esteban.baigts@epitech.eu>
2023-03-31 21:17:03 +0200

    main for the chocolatine

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** chocolatine [WSL : Ubuntu]
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+    return 0;
+}
